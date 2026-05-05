511a321145d9e4907bd6b51d2d06b2d16360838d
IIceBeer <diago.mainbranch.9895@gmail.com>
2026-05-05 23:44:42 +0530

    Delete components directory

--- components/site-background.tsx
+++
@@ -1,9 +0,0 @@
-export function SiteBackground() {
-  return (
-    <>
-      <div className="background-orb orb-one" />
-      <div className="background-orb orb-two" />
-      <div className="background-grid" aria-hidden="true" />
-    </>
-  );
-}
